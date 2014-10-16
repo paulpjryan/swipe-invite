065cd7a1efd44ced3de61ca703478233427563cd
paulpjryan <paulpjryan@gmail.com>
2014-10-16 16:27:14 -0400

    Got rid of DrawerActivity, made nav drawer use fragments again. App builds again

---
+++ source/SwipeInvite/app/src/main/java/com/team16/swipeinvite/GroupsFragment.java
@@ -0,0 +1,27 @@
+package com.team16.swipeinvite;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by PRyan on 10/16/2014.
+ */
+public class GroupsFragment extends Fragment {
+    public static final String ARG_PLANET_NUMBER = "planet_number";
+
+    public GroupsFragment() {
+        // Empty constructor required for fragment subclasses
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_group, container, false);
+
+        getActivity().setTitle("Groups");
+        return rootView;
+    }
+}
--- source/SwipeInvite/app/src/main/java/com/team16/swipeinvite/MainActivity.java
+++ source/SwipeInvite/app/src/main/java/com/team16/swipeinvite/MainActivity.java
@@ -1,33 +1,89 @@
 package com.team16.swipeinvite;
 
-import android.content.Context;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.FrameLayout;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends DrawerActivity
+public class MainActivity extends Activity
 {
+
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    private CharSequence mDrawerTitle;
+    private CharSequence mTitle;
+
+    private String[] drawerTitleList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // don’t set any content view here, since its already set in DrawerActivity
-        FrameLayout frameLayout = (FrameLayout)findViewById(R.id.activity_frame);
-        // inflate the custom activity layout
-        LayoutInflater layoutInflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View activityView = layoutInflater.inflate(R.layout.activity_main, null,false);
-        // add the custom layout of this activity to frame layout.
-        frameLayout.addView(activityView);
-        // now you can do all your other stuffs
+        setContentView(R.layout.activity_main);
+
+        mTitle = mDrawerTitle = getTitle();
+
+        //Names of tabs in the drawer
+        drawerTitleList = new String[]{"Events", "Groups", "Test"};
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // set a custom shadow that overlays the main content when the drawer opens
+        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+        // set up the drawer's list view with items and click listener
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, drawerTitleList));
+
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+
+        // enable ActionBar app icon to behave as action to toggle nav drawer
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+
+        // ActionBarDrawerToggle ties together the the proper interactions
+        // between the sliding drawer and the action bar app icon
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                mDrawerLayout,         /* DrawerLayout object */
+                R.drawable.ic_drawer,  /* nav drawer image to replace 'Up' caret */
+                R.string.drawer_open,  /* "open drawer" description for accessibility */
+                R.string.drawer_close  /* "close drawer" description for accessibility */
+        ) {
+            public void onDrawerClosed(View view) {
+                getActionBar().setTitle(mTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                getActionBar().setTitle(mDrawerTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        if (savedInstanceState == null) {
+            selectItem(0);
+
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.sample, menu);
+        getMenuInflater().inflate(R.menu.test, menu);
         return true;
     }
 
@@ -41,6 +97,37 @@ public class MainActivity extends DrawerActivity
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    /* The click listner for ListView in the navigation drawer */
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    private void selectItem(int position) {
+        // update the main content by replacing fragments
+        Fragment fragment;
+       //switch (position) {
+       //    case 2:
+        if(position == 1) {
+            fragment = new GroupsFragment();
+        }
+        else
+        {
+            fragment = new EventsFragment();
+        }
+       //        break;
+       //}
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+
+        // update selected item and title, then close the drawer
+        mDrawerList.setItemChecked(position, true);
+        setTitle(drawerTitleList[position]);
+        mDrawerLayout.closeDrawer(mDrawerList);
     }
 }
 
