35f773b99c9c1c3dd1c6a155cb28d0af98dbbae0
paulpjryan <paulpjryan@gmail.com>
2014-11-13 20:55:51 -0500

    Hide soft keyboard when drawer opens

--- source/SwipeInvite/app/src/main/java/com/team16/swipeinvite/MainActivity.java
+++ source/SwipeInvite/app/src/main/java/com/team16/swipeinvite/MainActivity.java
@@ -1,5 +1,6 @@
 package com.team16.swipeinvite;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.Intent;
@@ -13,6 +14,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -103,6 +105,10 @@ public class MainActivity extends ActionBarActivity {
             }
 
             public void onDrawerOpened(View drawerView) {
+                //Code to hide soft keyboard
+                InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+                imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
+
                 getSupportActionBar().setTitle(mDrawerTitle);
                 invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
             }
