cc9e5aba934ca6b7b05494218b9ec71e4882bf44
chen1366 <chen1366@purdue.edu>
2014-12-01 21:21:06 -0500

    ListView pulldown works
    Push changes for pulling down refresh

--- source/SwipeInvite/app/src/main/java/com/team16/swipeinvite/EventsFragment.java
+++ source/SwipeInvite/app/src/main/java/com/team16/swipeinvite/EventsFragment.java
@@ -16,6 +16,7 @@ import android.widget.ListView;
 public class EventsFragment extends Fragment {
 
     private EventsAdapter mEventsAdapter;
+    RefreshableView refreshableView;
 
     public EventsFragment() {
         // Empty constructor required for fragment subclasses
@@ -53,9 +54,20 @@ public class EventsFragment extends Fragment {
         //TextView mTextView;
         //mTextView = (TextView)getView().findViewById(R.id.textViewGroupsFragment);
         //mTextView.setText("Current User: " + BaasUser.current().getName() + " With PW: " + BaasUser.current().getPassword());
-
+        refreshableView = (RefreshableView) rootView.findViewById(R.id.refreshable_view);
         ListView listView = (ListView) rootView.findViewById(R.id.listview_events);
         listView.setAdapter(mEventsAdapter);
+        refreshableView.setOnRefreshListener(new RefreshableView.PullToRefreshListener() {
+            @Override
+            public void onRefresh() {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                refreshableView.finishRefreshing();
+            }
+        }, 0);
 
         //Give the main activity the group adapter
         ((MainActivity) getActivity()).setEventsAdapter(mEventsAdapter);
--- source/SwipeInvite/app/src/main/java/com/team16/swipeinvite/GroupsFragment.java
+++ source/SwipeInvite/app/src/main/java/com/team16/swipeinvite/GroupsFragment.java
@@ -24,6 +24,7 @@ public class GroupsFragment extends Fragment {
     private static final String LOG_TAG = "GROUP_FRAGMENT";
 
     private GroupsAdapter mArrayAdapter;
+    RefreshableView refreshableView;
 
     private Model m;
 
@@ -48,10 +49,21 @@ public class GroupsFragment extends Fragment {
         //TextView mTextView;
         //mTextView = (TextView)getView().findViewById(R.id.textViewGroupsFragment);
         //mTextView.setText("Current User: " + BaasUser.current().getName() + " With PW: " + BaasUser.current().getPassword());
-
+        refreshableView = (RefreshableView) rootView.findViewById(R.id.refreshable_view);
         EditText inputSearch = (EditText) rootView.findViewById(R.id.et_search_group);
         ListView listView = (ListView) rootView.findViewById(R.id.listview_groups);
         listView.setAdapter(mArrayAdapter);
+        refreshableView.setOnRefreshListener(new RefreshableView.PullToRefreshListener() {
+            @Override
+            public void onRefresh() {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                refreshableView.finishRefreshing();
+            }
+        }, 0);
 
         //Give the main activity the group adapter
         ((MainActivity) getActivity()).setGroupsAdapter(mArrayAdapter);
